9583eb0241be83f6df770f8f98c65ac9c281294f
fajarfadillahh <fajaragustian979@gmail.com>
2022-02-05 21:49:28 +0700

    custom theme container, colors, fontFamily, fontWeight on tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,26 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      center: true,
+    },
+    colors: {
+      black: "#2e2e2e",
+      grey: "#676767",
+      white: "#fff",
+      red: "#f86061",
+      yellow: "#fdc55e",
+      green: "#03a66b",
+    },
+    fontFamily: {
+      body: "Poppins",
+    },
+    fontWeight: {
+      reguler: 400,
+      medium: 500,
+      semibold: 600,
+      bold: 700,
+    },
     extend: {},
   },
   plugins: [],
